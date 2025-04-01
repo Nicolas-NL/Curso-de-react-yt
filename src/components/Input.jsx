466f2638dfaf560d0accafd08a34d646a3b944f0
Nicolas-NL <nicolas-nl@hotmail.com>
2025-04-01 13:14:57 -0300

    commit inicial

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input(props) {
+  return (
+    <input
+      className="w-full p-2 rounded-md bg-slate-100 border border-sl border-slate-300 outline-slate-400 px-4 py-2 rounded-nd"
+      {...props}
+    />
+  );
+}
+
+export default Input;
